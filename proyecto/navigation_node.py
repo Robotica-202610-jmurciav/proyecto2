3ebd02d765718843c89e2cf069e1db965d6dace2
CamMV <j.murciav@uniandes.edu.co>
2026-04-27 16:45:07 -0500

    escena 5

--- proyecto/navigation_node.py
+++ proyecto/navigation_node.py
@@ -521,7 +521,7 @@ class NavigationNode(Node):
         self.numero_escena = numero
 
         # 1. Parsear escena
-        scene = self._parsear_escena(4)
+        scene = self._parsear_escena(5)
         if scene is None:
             return
         self.scene_data = scene
